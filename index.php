2579f8044243952bad30ee25817445f3b1674546
Yuri <yuri01.sp@gmail.com>
2023-06-19 18:00:50 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,58 @@
+ <?php
+  // Aqui você pode incluir qualquer lógica de processamento de dados ou outras funcionalidades em PHP
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Souza Consultoria</title>
+</head>
+<body>
+      <!-- Inclua o cabeçalho da página -->
+  <header>
+    <a href="#" class="logo"><img src="img/logoLaranja.svg" alt="Logo Laranja"></a>
+
+    <ul class="navlista">
+        <li><a href="#">Serviços</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+
+    <a href="#" id="numberHeader">(45) 9978-7572</a>
+  </header>
+
+  <!-- Inclua o conteúdo principal da página -->
+  <main>
+    <section class="landingPage">
+        <h1>TRANSFORME DESAFIOS EM OPORTUNIDADES E CONQUISTE O SUCESSO.</h1>
+        <img src="img/imageLandingPage.png" alt="">
+    </section>
+    <section class="servicosHome">
+        <div id="card1"></div>
+        <div id="card2"></div>
+        <div id="card3"></div>
+
+        <div class="frases">
+
+        </div>
+
+        <div class="slider">
+
+        </div>
+    </section>
+
+  </main>
+
+  <!-- Inclua o rodapé da página -->
+  <footer>
+    <!-- Aqui você pode adicionar informações de contato, links para redes sociais, etc. -->
+    <p>&copy; <?php echo date('Y'); ?> Minha Empresa. Todos os direitos reservados.</p>
+  </footer>
+
+  <!-- Aqui você pode incluir scripts JavaScript ou links para arquivos externos -->
+</body>
+</html>
