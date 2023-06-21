551ad8cc31340dcde38c4fa4ae3a721a6200893d
Yuri <yuri01.sp@gmail.com>
2023-06-21 18:00:33 -0300

    Inserindo e estilizando a parte de serviços

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,20 +22,38 @@
         <li><a href="#">Contato</a></li>
     </ul>
 
-    <a href="#" id="numberHeader">(45) 9978-7572</a>
+    <a href="#" id="numberHeader">(45) 99978-7572</a>
   </header>
 
   <!-- Inclua o conteúdo principal da página -->
   <main>
     <section class="landingPage">
         <h1>TRANSFORME DESAFIOS EM <br>OPORTUNIDADES E CONQUISTE O <br>SUCESSO.</h1>
-        <img src="img/imageLandingPage.png" alt="">
+        <img src="img/imageLandingPage.png" alt="" id="imagemLP">
     </section>
     <section class="servicosHome">
-        <div id="card1"></div>
-        <div id="card2"></div>
-        <div id="card3"></div>
+      <div class="card">
+          <div id="card1">
+          <img src="img/desenvolvimentoEstrategia.jpg" alt="">
+            <h3>Desenvolvimento de <br> estratégias</h3>
+            <p>Minha Consultoria Estratégica ajuda empresas a alcançarem seu máximo potencial. Trabalho em parceria com sua equipe, realizando uma análise abrangente do mercado e identificando oportunidades-chave. Com base nessa análise, desenvolvo planos de negócios personalizados e estratégias de crescimento para orientar sua empresa rumo ao sucesso. Com minha expertise estratégica, posiciono sua empresa de forma competitiva e aproveito ao máximo as oportunidades disponíveis. Juntos, alcançaremos resultados excepcionais.</p>
+            <button>Mais</button>
+          </div>
 
+          <div id="card2">
+          <img src="img/otimizacaoProcessos.jpg" alt="">
+            <h3>Otimização de <br> processos</h3>
+            <p>Maximize o potencial da sua empresa com minha consultoria estratégica. Identifico oportunidades-chave após uma análise de mercado detalhada e crio planos de negócios personalizados. Minha expertise estratégica posiciona sua empresa de forma competitiva, permitindo que você aproveite ao máximo as oportunidades disponíveis. Juntos, alcançaremos resultados excepcionais.</p>
+            <button>SAIBA MAIS</button>
+          </div>
+
+          <div id="card3">
+            <img src="img/consultoriaTi.jpg" alt="">
+            <h3>Consultoria em <br> tecnologia</h3>
+            <p>Minha Consultoria em Tecnologia ajuda empresas a escolher e implementar as soluções tecnológicas certas para suas necessidades específicas. Com experiência no setor, avalio seu ambiente tecnológico, identifico áreas de melhoria e ofereço recomendações personalizadas. Desde a implementação de sistemas até a escolha de software especializado e melhoria da segurança cibernética, estou aqui para ajudar. Com minha orientação especializada, você aproveitará ao máximo a tecnologia, obtendo melhores resultados, maior eficiência e vantagem competitiva no mercado.</p>
+            <button>Mais</button>
+          </div>
+        </div>
         <div class="frases">
 
         </div>
