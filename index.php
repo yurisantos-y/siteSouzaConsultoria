d703b1815be9fd61e61dd440ffa186393364890e
yuri <yuri01.sp@gmail.com>
2023-08-19 08:20:44 -0300

    Editando o focus do input no footer

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ error_reporting(E_ALL);
         <ul class="navlista">
             <li><a href="#">Serviços</a></li>
             <li><a href="#">Sobre</a></li>
-            <li><a href="#">Contato</a></li>
+            <li><a href="#">Download</a></li>
         </ul>
         <a href="#" id="numberHeader">(45) 99978-7572</a>
     </header>
