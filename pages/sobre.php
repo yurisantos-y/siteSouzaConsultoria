1561900655a607acc8809f69c0675ccfc49e6689
yuri <yuri01.sp@gmail.com>
2023-09-01 14:13:12 -0300

    Finalizando o footer da pagina sobre

--- pages/sobre.php
+++ pages/sobre.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../style/sobre.css">
+    <link rel="stylesheet" href="../style/style.css">
     <link rel="shortcut icon" href="../img/icon.ico" type="image/x-icon">
     <title>Prospere | Consultoria</title>
 </head>
@@ -29,43 +30,38 @@
     </header>
     <main>
 
-        <div class="conteudo">
+        <section class="sobre">
+            <div class="conteudo">
 
-            <p class="paragrafo">
-                <span class="destaque">Olá, sou Natal</span> Um empresário de sucesso com uma sólida experiência na
-                indústria da tecnologia. Na Computec, minha empresa de consultoria líder, desenvolvemos e implementamos
-                um avançado sistema de gerenciamento de entrada e saída. Nosso sistema é projetado para otimizar e
-                aprimorar o fluxo de pessoas e recursos dentro das organizações, oferecendo soluções eficientes e
-                seguras para o controle de acesso. <br>
-                <br>
-                Com nossa expertise em tecnologia e consultoria, ajudamos empresas a melhorar sua segurança, eficiência
-                operacional e satisfação do cliente por meio de um gerenciamento de entrada e saída inteligente. Nossa
-                abordagem personalizada e soluções inovadoras permitem que as empresas tenham um controle mais efetivo
-                do fluxo de pessoas, monitorando a entrada e saída de funcionários, visitantes e mercadorias de forma
-                eficiente e precisa. Na Computec, estamos comprometidos em fornecer sistemas de gerenciamento de entrada
-                e saída de ponta, que impulsionam o sucesso e a produtividade de nossos clientes.
-            </p>
+                <p class="paragrafo">
+                    <span class="destaque">Olá, sou Natal</span> Um empresário de sucesso com uma sólida experiência na
+                    indústria da tecnologia. Na Computec, minha empresa de consultoria líder, desenvolvemos e
+                    implementamos
+                    um avançado sistema de gerenciamento de entrada e saída. Nosso sistema é projetado para otimizar e
+                    aprimorar o fluxo de pessoas e recursos dentro das organizações, oferecendo soluções eficientes e
+                    seguras para o controle de acesso. <br>
+                    <br>
+                    Com nossa expertise em tecnologia e consultoria, ajudamos empresas a melhorar sua segurança,
+                    eficiência
+                    operacional e satisfação do cliente por meio de um gerenciamento de entrada e saída inteligente.
+                    Nossa
+                    abordagem personalizada e soluções inovadoras permitem que as empresas tenham um controle mais
+                    efetivo
+                    do fluxo de pessoas, monitorando a entrada e saída de funcionários, visitantes e mercadorias de
+                    forma
+                    eficiente e precisa. Na Computec, estamos comprometidos em fornecer sistemas de gerenciamento de
+                    entrada
+                    e saída de ponta, que impulsionam o sucesso e a produtividade de nossos clientes.
+                </p>
 
 
 
-            <div class="imagemNatal">
-                <img src="../img/natal.jpg" alt="Natal">
+                <div class="imagemNatal">
+                    <img src="../img/natal.jpg" alt="Natal">
+                </div>
             </div>
-        </div>
+        </section>
 
-        <script src="../script/scroll.js"></script>
-        <script src="../script/menuMobile.js"></script>
-        <script src="../ck/build/ckeditor.js"></script>
-        <script src="../script/ckeditor.js"></script>
-        <script src="../script/frases.js"></script>
-        <script src="../script/slides.js"></script>
-        <script>
-        initSlider({
-            autoPlay: true,
-            startAtIndex: 0,
-            timeInterval: 2000
-        })
-        </script>
 
         <footer>
             <div class="footer-content">
@@ -119,6 +115,19 @@
 
 
 
+        <script src="../script/scroll.js"></script>
+        <script src="../script/menuMobile.js"></script>
+        <script src="../ck/build/ckeditor.js"></script>
+        <script src="../script/ckeditor.js"></script>
+        <script src="../script/frases.js"></script>
+        <script src="../script/slides.js"></script>
+        <script>
+        initSlider({
+            autoPlay: true,
+            startAtIndex: 0,
+            timeInterval: 2000
+        })
+        </script>
     </main>
 </body>
 
